b25c97050ee7b0f4b2d6166773f9b061c2b0021a
jesus m. rodriguez <jmrodri@gmail.com>
2017-05-21 15:46:15 -0400

    create web server

--- hello.go
+++ hello.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func PostData(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+}
 
 func main() {
 	fmt.Println("hello from ultraduster")
-	fmt.Println("If you are seeing this, you are looking at release 0.0.2")
+	http.HandleFunc("/", PostData)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
